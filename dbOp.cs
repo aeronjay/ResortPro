7de23cd27305f798085566d90df0df977d8cf4cb
Aeron Bulatao <bulataoaeronjay@gmail.com>
2024-04-10 21:08:30 +0800

    add database file, connection string to dbOp

---
+++ dbOp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace ResortPro
+{
+    internal static class dbOp
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                string databasePath = @"..\..\db\ResortProDB.mdb";
+
+                string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+                string fullPath = Path.GetFullPath(Path.Combine(startupPath, databasePath));
+
+                string connectionString = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={fullPath};Jet OLEDB:Database Password=LesterLoveEulen2024;";
+
+                return connectionString;
+            }
+        }
+        
+    }
+}
