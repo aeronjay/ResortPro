d1f27a5279b223dd98f49c8793e8ea13731684fc
Aeron Bulatao <bulataoaeronjay@gmail.com>
2024-04-13 21:09:22 +0800

    form2 add shits

--- loggedIn.Designer.cs
+++ loggedIn.Designer.cs
@@ -28,19 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.dashboard = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dashboardLabel = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.dashboard.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(9)))), ((int)(((byte)(9)))));
+            this.panelMenu.Controls.Add(this.dashboard);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 766);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // dashboard
+            // 
+            this.dashboard.Controls.Add(this.kryptonButton1);
+            this.dashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dashboard.Location = new System.Drawing.Point(0, 100);
+            this.dashboard.Name = "dashboard";
+            this.dashboard.Size = new System.Drawing.Size(220, 56);
+            this.dashboard.TabIndex = 1;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 100);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.CornerRoundingRadius = 30F;
+            this.kryptonButton1.Location = new System.Drawing.Point(34, 6);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.PaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.kryptonButton1.Size = new System.Drawing.Size(149, 45);
+            this.kryptonButton1.StateCommon.Border.DrawBorders = ((Krypton.Toolkit.PaletteDrawBorders)((((Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonButton1.StateCommon.Border.Rounding = 30F;
+            this.kryptonButton1.TabIndex = 0;
+            this.kryptonButton1.Values.Text = "Dashboard";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dashboardLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(220, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(934, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // dashboardLabel
+            // 
+            this.dashboardLabel.AutoSize = true;
+            this.dashboardLabel.Location = new System.Drawing.Point(51, 47);
+            this.dashboardLabel.Name = "dashboardLabel";
+            this.dashboardLabel.Size = new System.Drawing.Size(59, 13);
+            this.dashboardLabel.TabIndex = 0;
+            this.dashboardLabel.Text = "Dashboard";
             // 
             // loggedIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 485);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(223)))), ((int)(((byte)(215)))));
+            this.ClientSize = new System.Drawing.Size(1154, 766);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelMenu);
+            this.CornerRoundingRadius = 20F;
             this.Name = "loggedIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StateCommon.Border.DrawBorders = ((Krypton.Toolkit.PaletteDrawBorders)((((Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.Rounding = 20F;
             this.Text = " ";
+            this.panelMenu.ResumeLayout(false);
+            this.dashboard.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel dashboard;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label dashboardLabel;
     }
 }
--- loggedIn.cs
+++ loggedIn.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
+using Krypton.Toolkit;
 
 namespace ResortPro
 {
-    public partial class loggedIn : Form
+    public partial class loggedIn : KryptonForm
     {
         public loggedIn()
         {
             InitializeComponent();
+            
         }
+
     }
 }
