af59ba2c51cbeb7818e904e74450d85ef9ef32b1
lester <xxlessu456@gmail.com>
2024-07-02 11:37:55 +0800

    bading

--- Calendar.Designer.cs
+++ Calendar.Designer.cs
@@ -49,18 +49,20 @@
             // daycontainer
             // 
             this.daycontainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(95)))), ((int)(((byte)(45)))));
-            this.daycontainer.Location = new System.Drawing.Point(12, 94);
+            this.daycontainer.Location = new System.Drawing.Point(16, 116);
+            this.daycontainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.daycontainer.Name = "daycontainer";
-            this.daycontainer.Size = new System.Drawing.Size(970, 475);
+            this.daycontainer.Size = new System.Drawing.Size(1293, 585);
             this.daycontainer.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(43, 59);
+            this.label1.Location = new System.Drawing.Point(57, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.Size = new System.Drawing.Size(80, 22);
             this.label1.TabIndex = 3;
             this.label1.Text = "Sunday";
             // 
@@ -68,9 +70,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(177, 59);
+            this.label2.Location = new System.Drawing.Point(236, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.Size = new System.Drawing.Size(88, 22);
             this.label2.TabIndex = 4;
             this.label2.Text = "Monday";
             // 
@@ -78,9 +81,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(320, 59);
+            this.label3.Location = new System.Drawing.Point(427, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.Size = new System.Drawing.Size(86, 22);
             this.label3.TabIndex = 5;
             this.label3.Text = "Tuesday";
             // 
@@ -88,9 +92,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(441, 59);
+            this.label4.Location = new System.Drawing.Point(588, 73);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.Size = new System.Drawing.Size(120, 22);
             this.label4.TabIndex = 6;
             this.label4.Text = "Wednesday";
             // 
@@ -98,9 +103,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(594, 59);
+            this.label5.Location = new System.Drawing.Point(792, 73);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.Size = new System.Drawing.Size(92, 22);
             this.label5.TabIndex = 7;
             this.label5.Text = "Thursday";
             // 
@@ -108,9 +114,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(737, 59);
+            this.label6.Location = new System.Drawing.Point(983, 73);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.Size = new System.Drawing.Size(65, 22);
             this.label6.TabIndex = 8;
             this.label6.Text = "Friday";
             // 
@@ -118,9 +125,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(871, 59);
+            this.label7.Location = new System.Drawing.Point(1161, 73);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(73, 20);
+            this.label7.Size = new System.Drawing.Size(94, 22);
             this.label7.TabIndex = 9;
             this.label7.Text = "Saturday";
             // 
@@ -128,9 +136,10 @@
             // 
             this.LBDATE.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LBDATE.ForeColor = System.Drawing.Color.White;
-            this.LBDATE.Location = new System.Drawing.Point(273, 9);
+            this.LBDATE.Location = new System.Drawing.Point(364, 11);
+            this.LBDATE.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LBDATE.Name = "LBDATE";
-            this.LBDATE.Size = new System.Drawing.Size(341, 35);
+            this.LBDATE.Size = new System.Drawing.Size(455, 43);
             this.LBDATE.TabIndex = 10;
             this.LBDATE.Text = "MONTH YEAR";
             this.LBDATE.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -142,9 +151,10 @@
             this.guna2GradientPanel1.Controls.Add(this.closeButton);
             this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2GradientPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2GradientPanel1.Name = "guna2GradientPanel1";
             this.guna2GradientPanel1.ShadowDecoration.Parent = this.guna2GradientPanel1;
-            this.guna2GradientPanel1.Size = new System.Drawing.Size(993, 53);
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(1324, 65);
             this.guna2GradientPanel1.TabIndex = 10;
             // 
             // closeButton
@@ -157,9 +167,10 @@
             this.closeButton.IconColor = System.Drawing.Color.White;
             this.closeButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.closeButton.IconSize = 30;
-            this.closeButton.Location = new System.Drawing.Point(946, 0);
+            this.closeButton.Location = new System.Drawing.Point(1261, 0);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(47, 53);
+            this.closeButton.Size = new System.Drawing.Size(63, 65);
             this.closeButton.TabIndex = 0;
             this.closeButton.UseVisualStyleBackColor = true;
             // 
@@ -179,10 +190,11 @@
             this.btnPrev.ForeColor = System.Drawing.Color.White;
             this.btnPrev.HoverState.FillColor = System.Drawing.Color.LightGreen;
             this.btnPrev.HoverState.Parent = this.btnPrev;
-            this.btnPrev.Location = new System.Drawing.Point(702, 579);
+            this.btnPrev.Location = new System.Drawing.Point(936, 713);
+            this.btnPrev.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrev.Name = "btnPrev";
             this.btnPrev.ShadowDecoration.Parent = this.btnPrev;
-            this.btnPrev.Size = new System.Drawing.Size(123, 45);
+            this.btnPrev.Size = new System.Drawing.Size(164, 55);
             this.btnPrev.TabIndex = 31;
             this.btnPrev.Text = "Previous";
             // 
@@ -197,19 +209,20 @@
             this.btnNext.ForeColor = System.Drawing.Color.White;
             this.btnNext.HoverState.FillColor = System.Drawing.Color.LightGreen;
             this.btnNext.HoverState.Parent = this.btnNext;
-            this.btnNext.Location = new System.Drawing.Point(847, 579);
+            this.btnNext.Location = new System.Drawing.Point(1129, 713);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNext.Name = "btnNext";
             this.btnNext.ShadowDecoration.Parent = this.btnNext;
-            this.btnNext.Size = new System.Drawing.Size(117, 41);
+            this.btnNext.Size = new System.Drawing.Size(156, 50);
             this.btnNext.TabIndex = 32;
             this.btnNext.Text = "Next";
             // 
             // Calendar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(233)))), ((int)(((byte)(225)))));
-            this.ClientSize = new System.Drawing.Size(993, 640);
+            this.ClientSize = new System.Drawing.Size(1324, 788);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.guna2GradientPanel1);
@@ -222,6 +235,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.daycontainer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Calendar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calendar";
