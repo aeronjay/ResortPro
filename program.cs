9ff3da697ef3370dc657f08a92dd60770eed58fd
lester <xxlessu456@gmail.com>
2024-07-07 20:11:24 +0800

    WALA LANG

--- program.cs
+++ program.cs
@@ -21,7 +21,7 @@ namespace ResortPro1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new manage_staff());
+            Application.Run(new loginForm());
 
         }
     }
